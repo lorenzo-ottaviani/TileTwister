ca46fd05b00daff2616c68366a59a643ee231fb7
Abdelrahmane-Amiri <abdelrahmane.amiri@laplateforme.io>
2025-12-18 20:11:04 +0100

    Upgrade drawGrid for best graphics

--- src/Display.cpp
+++ src/Display.cpp
@@ -27,21 +27,33 @@ void Display::reset(){
 }
 
 void Display::drawGrid(){
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-    int numberCase = 4;
-    int sizeCase = 100;
-
-    for (int i = 0; i <= numberCase+1; ++i) {
-        int x = i * sizeCase;
-        SDL_RenderDrawLine(renderer, x, 100, x, windowSize -100);
+    // Fond général
+    SDL_SetRenderDrawColor(renderer, 250, 248, 239, 255);
+    SDL_RenderClear(renderer);
+    
+    const int sizeGrid = 4;
+    const int margin = 100;
+    const int cellPadding = 15;
+    const int spaceDisponible = windowSize - 2 * margin;
+    const int cellSize = (spaceDisponible - cellPadding * (sizeGrid + 1)) / sizeGrid;
+    
+    SDL_Rect gridBg = {margin, margin, spaceDisponible, spaceDisponible};
+    SDL_SetRenderDrawColor(renderer, 187, 173, 160, 255);
+    SDL_RenderFillRect(renderer, &gridBg);
+    
+    SDL_SetRenderDrawColor(renderer, 205, 193, 180, 255);
+    for (int row = 0; row < sizeGrid; ++row) {
+        for (int col = 0; col < sizeGrid; ++col) {
+            SDL_Rect cell = {
+                margin + cellPadding + col * (cellSize + cellPadding),
+                margin + cellPadding + row * (cellSize + cellPadding),
+                cellSize,
+                cellSize
+            };
+            SDL_RenderFillRect(renderer, &cell);
+        }
     }
-
-    for (int i = 0; i <= numberCase+1; ++i) {
-        int y = i* sizeCase;
-        SDL_RenderDrawLine(renderer, 100, y, windowSize -100, y);;
-    }
-
+    
     SDL_RenderPresent(renderer);
 }
 
